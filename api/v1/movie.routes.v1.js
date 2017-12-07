58ee2d5275d9937f9ad5fa76061dec9e4a4a3823
Thimo Koolen <tkoolen1@student.avans.nl>
2017-12-07 16:44:54 +0100

    Omg het werkt.

--- api/v1/movie.routes.v1.js
+++ api/v1/movie.routes.v1.js
@@ -7,19 +7,6 @@ var Movie = require('../../model/movie.model');
 routes.get('/movies', function (req, res) {
 	Movie.find({})
 		.then((movies) => {
-			neo4j.session.run('MATCH (n) RETURN n')
-				.then((result) => {
-					var resArr = [];
-					result.records.forEach(function (record) {
-						resArr.push({
-							name: record._fields[0].properties.name,
-							mongoId: record._fields[0].properties.mongoId
-						});
-					});
-					
-					console.log('Result');
-					console.log(resArr.json());
-				});
 			res.status(200).json(movies);
 		})
 		.catch((error) => res.status(400).json(error));
@@ -35,6 +22,36 @@ routes.get('/movies/:id', function (req, res) {
 		.catch((error) => res.status(400).json(error));
 });
 
+routes.get('/movies/:id/actors', function (req, res) {
+	const id = req.params.id;
+	
+	neo4j.session
+		.run("MATCH (a:Actor)-[:ACTS_IN]->(m:Movie{mongoId: {idParam}}) RETURN a", {idParam: id})
+		.then((result) => {
+			var actorArr = [];
+			var movieName = '';
+			
+			result.records.forEach((record) => {
+				actorArr.push({
+					name: record._fields[0].properties.name
+				});
+			});
+			
+			Movie.find({'_id': id})
+				.then((movie) => {
+					movieName = movie[0].name;
+					
+					const response = {
+						movieName: movieName,
+						actors: actorArr
+					};
+					
+					neo4j.close();
+					res.status(200).json(response);
+				});
+		});
+});
+
 routes.post('/movies', function (req, res) {
 	const body = req.body;
 	
@@ -44,7 +61,10 @@ routes.post('/movies', function (req, res) {
 		.then((movie) => {
 			const movieId = movie._id + '';
 			neo4j.session
-				.run("CREATE (movie:Movie{name: {nameParam}, mongoId: {idParam}}) RETURN movie", {nameParam: body.name, idParam: movieId})
+				.run("CREATE (movie:Movie{name: {nameParam}, mongoId: {idParam}}) RETURN movie", {
+					nameParam: body.name,
+					idParam: movieId
+				})
 				.then(() => {
 					neo4j.close();
 					res.status(200).json(movie)
@@ -52,6 +72,28 @@ routes.post('/movies', function (req, res) {
 				.catch((error) => res.status(400).json(error.message));
 		})
 		.catch((error) => res.status(400).json(error));
+});
+
+routes.post('/movies/:id/actors', function (req, res) {
+	const id = req.params.id;
+	const body = req.body;
+	
+	const name = body.name;
+	
+	neo4j.session
+		.run("CREATE (a:Actor{name: {nameParam}}) RETURN a", {nameParam: name})
+		.then(() => {
+			neo4j.session
+				.run("MATCH (a:Actor{name: {nameParam}}), (m:Movie{mongoId: {idParam}}) CREATE UNIQUE (a)-[:ACTS_IN]->(m) RETURN a, m", {
+					nameParam: name,
+					idParam: id
+				})
+				.then((result) => {
+					neo4j.close();
+					res.status(200).json(result);
+				})
+				.catch((error) => res.status(400).json(error));
+		})
 });
 
 routes.put('/movies/:id', function (req, res) {
